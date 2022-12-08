2b3ede2a86f65d3b2dbddcbc2b937275d986ec35
Kevin-Cool <kevincoolXD@gmail.com>
2022-12-08 20:54:42 +0100

    automated update

--- analyse_data.py
+++ analyse_data.py
@@ -13,8 +13,7 @@ def main():
     df  = pd.read_csv('C:\\Users\\kevin\\Documents\\school\\2022-2023\\semester 1\\3 Linux for Data Scientists\\Proof-of-concept geautomatiseerde data workflow\\cleandata\\cleandata.csv')
 
     # change the format of data to a more usefull format for graphs
-    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.normalize()
-    df = df.astype({'timestamp': 'datetime64[ns]'})
+    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.normalize().astype('datetime64[ns]')
 
     df_daily = df[['value','timestamp','is_indoor']].resample('D', on='timestamp').mean()
     df_daily_indoor = df.query("is_indoor == 1")[['value','timestamp','is_indoor']].resample('D', on='timestamp').mean()
@@ -24,23 +23,22 @@ def main():
 
     # create plot for inside/outside
     plot = sns.lineplot(data=df_daily_complex, x="timestamp", y="value",hue="is_indoor")
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(base=1))
     plt.legend(title='Air quality', loc='upper left', labels=['outside', 'inside'])
     plt.ylabel('Air quality')
+    plt.xticks(rotation=30)
+    plt.show()
     fig = plot.get_figure()
-    fig.savefig('analysedata/separate.png')  
+    fig.savefig('saperate.png')  
     plt.cla()
     plt.clf()
     
     #  create plot for inside/outside combined
     plot2 = sns.lineplot(data=df_daily, x="timestamp", y="value")
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(base=1))
     plt.ylabel('Air quality')
-    #plt.show()
+    plt.xticks(rotation=30)
+    plt.show()
     fig = plot2.get_figure()
-    fig.savefig('analysedata/combined.png')  
+    fig.savefig('combined.png') 
     plt.cla()
     plt.clf()
 
